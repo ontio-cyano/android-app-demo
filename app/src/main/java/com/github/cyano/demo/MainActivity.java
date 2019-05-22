7eaf1729678deb5ad1865b547fb18b748c553daf
zg919506719 <zg919506719@163.com>
2019-05-22 09:44:16 +0800

    add TransactionUtil

--- app/src/main/java/com/github/cyano/demo/MainActivity.java
+++ app/src/main/java/com/github/cyano/demo/MainActivity.java
@@ -7,13 +7,21 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Base64;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tvLogin = findViewById(R.id.sample_text);
         TextView tvInvoke = findViewById(R.id.sample_text1);
+        final EditText etPayer = findViewById(R.id.et_payer);
+        final EditText etReceiver = findViewById(R.id.et_receiver);
+        final EditText etAmount = findViewById(R.id.et_amount);
+        final RadioButton rbOnt = findViewById(R.id.rb_ont);
+        final RadioButton rbOng = findViewById(R.id.rb_ong);
+
         tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,8 +49,31 @@ public class MainActivity extends AppCompatActivity {
         tvInvoke.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String data = "{\"action\":\"invoke\",\"version\":\"v1.0.0\",\"id\":\"10ba038e-48da-487b-96e8-8d3b99b6d18a\",\"params\":{\"login\":true,\"qrcodeUrl\":\"http://101.132.193.149:4027/qrcode/AUr5QUfeBADq6BMY6Tp5yuMsUNGpsD7nLZ\",\"message\":\"will pay 1 ONT in this transaction\",\"callback\":\"http://101.132.193.149:4027/invoke/callback\"}}";
-                sendData(data);
+                if (!TextUtils.isEmpty(etPayer.getText().toString()) && !TextUtils.isEmpty(etReceiver.getText().toString()) && !TextUtils.isEmpty(etAmount.getText().toString())) {
+                    String payer = etPayer.getText().toString();
+                    String receiver = etReceiver.getText().toString();
+                    String amountStr = etAmount.getText().toString();
+                    Map param;
+                    if (rbOng.isChecked()) {
+                        param = TransactionUtil.transferParam(rbOng.getText().toString(), payer, receiver, (long) Double.parseDouble(amountStr) * 1000000000);
+                    } else {
+                        param = TransactionUtil.transferParam(rbOnt.getText().toString(), payer, receiver, Long.parseLong(amountStr));
+                    }
+                    try {
+                        JSONObject jsonObject = new JSONObject();
+                        jsonObject.put("action", "invoke");
+                        jsonObject.put("version", "v1.0.0");
+                        jsonObject.put("id", "10ba038e-48da-487b-96e8-8d3b99b6d18a");
+                        JSONObject jsonParam = new JSONObject();
+                        jsonParam.put("invokeConfig", new JSONObject(param));
+                        jsonParam.put("callback", "http://101.132.193.149:4027/invoke/callback");
+                        jsonParam.put("message", "will pay 1 ONT in this transaction");
+                        jsonObject.put("params", jsonParam);
+                        sendData(jsonObject.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
@@ -50,12 +87,12 @@ public class MainActivity extends AppCompatActivity {
             intent.setData(Uri.parse("ontprovider://ont.io?param=" + sendData));
             intent.addCategory("android.intent.category.DEFAULT");
             startActivity(intent);
-        }else {
+        } else {
             Toast.makeText(MainActivity.this, "Not Installed", Toast.LENGTH_LONG).show();
         }
     }
 
-    private static String PACK_NAME = "com.github.ont.cyanowallet";
+    private static String PACK_NAME = "com.github.ontio.onto";
 
     public static boolean isInstallApp(Context context) {
         final PackageManager packageManager = context.getPackageManager();// 获取packagemanager
---
+++ app/src/main/java/com/github/cyano/demo/TransactionUtil.java
@@ -0,0 +1,97 @@
+package com.github.cyano.demo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TransactionUtil {
+//    String requestOrderUrl = "http://127.0.0.1:10332/api/v1/ontid/request/order";
+//    String ontid = "did:ont:ANS9JnoER5WqcE75jHeYZAuSWRvTjP69WH";
+
+    public static Map transferParam(String type, String payer, String toAddress, long amount) {
+        String contractHash = "";
+        switch (type) {
+            case "ong":
+                contractHash = "0200000000000000000000000000000000000000";
+                break;
+            case "ont":
+                contractHash = "0100000000000000000000000000000000000000";
+//                0100000000000000000000000000000000000000
+                break;
+        }
+        String method = "transfer";
+        return getParams(contractHash, method, payer, toAddress, amount, 20000);
+        //预执行检验
+//            OntSdk ontSdk = getOntSdk();
+//            Map tmp = new HashMap();
+//            tmp.put("invokeConfig", params);
+//            // byte[] bys = java.util.Base64.getDecoder().decode(order.getToken());
+//            req.put("action", "invoke");
+//            req.put("params", tmp);
+//                Transaction[] tansactions = ontSdk.makeTransactionByJson(JSON.toJSONString(req));
+//                ontSdk.addSign(tansactions[0], account);
+//                ontSdk.getConnect().sendRawTransactionPreExec(tansactions[0].toHexString());
+//                System.exit(0);
+
+    }
+
+
+    public static Map getParams(String contractHash, String method, String payer, String toAddress, long amount, long gasLimit) {
+        List argsList = new ArrayList();
+        Map arg0 = new HashMap();
+        arg0.put("name", "from");
+        arg0.put("value", "Address:" + payer);
+        Map arg1 = new HashMap();
+        arg1.put("name", "to");
+        arg1.put("value", "Address:" + toAddress);
+        Map arg2 = new HashMap();
+        arg2.put("name", "amount");
+        arg2.put("value", "Long:"+amount);
+        argsList.add(arg0);
+        argsList.add(arg1);
+        argsList.add(arg2);
+
+        Map str = new HashMap();
+        Map parms = new HashMap();
+        Map invokeConfig = new HashMap();
+        List functions = new ArrayList();
+        Map function = new HashMap();
+
+        function.put("operation", method);
+        function.put("args", argsList);
+
+        functions.add(function);
+
+        invokeConfig.put("contractHash", contractHash);
+        invokeConfig.put("functions", functions);
+        invokeConfig.put("payer", payer);
+        invokeConfig.put("gasLimit", gasLimit);
+        invokeConfig.put("gasPrice", 500);
+
+        parms.put("invokeConfig", invokeConfig);
+
+        str.put("action", "invoke");
+        str.put("params", parms);
+        return invokeConfig;
+    }
+
+
+//    public static OntSdk getOntSdk() throws Exception {
+////        String ip = "http://127.0.0.1";
+//        String ip = "http://polaris5.ont.io";
+////        String ip = "http://139.219.129.55";
+////        String ip = "http://101.132.193.149";
+//        String restUrl = ip + ":" + "20334";
+//        String rpcUrl = ip + ":" + "20336";
+//        String wsUrl = ip + ":" + "20335";
+//
+//        OntSdk wm = OntSdk.getInstance();
+//        wm.setRpc(rpcUrl);
+//        wm.setRestful(restUrl);
+//        wm.setDefaultConnect(wm.getRestful());
+//
+//        wm.openWalletFile("NativeOntIdDemo.json");
+//        return wm;
+//    }
+}
